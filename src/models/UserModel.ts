1dd2b4269f01c56cf2f6f928d4107e387618e194
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-02-27 16:48:45 -0300

    Created UserModel

---
+++ src/models/UserModel.ts
@@ -0,0 +1,79 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import sequelize from "../config/database";
+
+interface UserAttributes {
+  userId: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+  admin: boolean;
+}
+
+interface UserCreationAttributes
+  extends Optional<UserAttributes, "userId" | "createdAt" | "updatedAt"> {}
+
+class UserModel extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+  public userId!: string;
+  public name!: string;
+  public email!: string;
+  public password!: string;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+  public deletedAt?: Date;
+  public admin!: boolean;
+}
+
+UserModel.init(
+  {
+    userId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    admin: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+    timestamps: true,
+    paranoid: true,
+    modelName: "UserModel",
+  }
+);
+
+export default UserModel;
