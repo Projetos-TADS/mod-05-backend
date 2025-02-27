0d1bd9f92d64409fc909da8b43399ce0ef6dd794
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-02-27 17:30:35 -0300

    Created userRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,11 @@
 import express from "express";
 import sequelize from "./config/database";
-import UserModel from "./models/UserModel";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
 const port = 3000;
 app.use(express.json());
-
-app.get("/users", async (req, res) => {
-  const users = await UserModel.findAll();
-  res.send(users);
-});
-
-app.get("/users/:id", async (req, res) => {
-  const { id } = req.params;
-  const user = await UserModel.findByPk(id);
-  if (!user) {
-    res.status(404).send("User not found.");
-  }
-  res.send(user);
-});
-
-app.post("/users", async (req, res) => {
-  const { name, email, password, admin } = req.body;
-  const user = await UserModel.create({ name, email, password, admin });
-  res.send(user);
-});
+app.use(userRoutes);
 
 sequelize
   .sync({ alter: true })
