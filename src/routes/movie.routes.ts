edec1cc461b67afe27810bdfe96f3b5d87189e52
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-07 14:09:58 -0300

    Corrige rota de movies

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -6,10 +6,13 @@ import movieControllers from "../controllers/movie.controllers";
 export const movieRoutes: Router = Router();
 
 movieRoutes.use(middlewares.verifyToken);
+
 movieRoutes.post("", middlewares.validateBody(movieCreateSchema), movieControllers.createMovie);
 movieRoutes.get("", movieControllers.getAllMovies);
+
 movieRoutes.use("/:movieId", middlewares.verifyMovieIdExists);
-movieRoutes.get("/:movieId", middlewares.verifyToken, movieControllers.getMovieById);
+
+movieRoutes.get("/:movieId", movieControllers.getMovieById);
 movieRoutes.patch(
   "/:movieId",
   middlewares.validateBody(movieUpdateSchema),
