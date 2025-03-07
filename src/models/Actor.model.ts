992ff3a026a3f09536c4a1d4a723892945c45996
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-07 17:32:53 -0300

    Cria model de actor

---
+++ src/models/Actor.model.ts
@@ -0,0 +1,41 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+import { ActorAttributes, ActorCreationAttributes } from "../interfaces";
+
+export class ActorModel
+  extends Model<ActorAttributes, ActorCreationAttributes>
+  implements ActorAttributes
+{
+  public actorId!: string;
+  public name!: string;
+  public birthDate!: Date;
+  public nationality!: string;
+}
+
+ActorModel.init(
+  {
+    actorId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    birthDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    nationality: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "actors",
+    timestamps: false,
+  }
+);
