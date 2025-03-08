47ce26f6cc45b1f35d2e8a4cad06f4cc307e21d2
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-08 15:25:07 -0300

    Cria middleware para Directors

---
+++ src/middlewares/verifyDirectorIdExists.middleware.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors";
+import { DirectorModel } from "../models/Director.model";
+
+const verifyDirectorIdExists = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
+  let directorId: string | undefined = request.params.directorId || request.body.directorId;
+
+  const director: DirectorModel | null = await DirectorModel.findByPk(directorId);
+
+  if (!director) throw new AppError("Director not found", 404);
+
+  response.locals = { ...response.locals, director };
+
+  return next();
+};
+
+export default verifyDirectorIdExists;
