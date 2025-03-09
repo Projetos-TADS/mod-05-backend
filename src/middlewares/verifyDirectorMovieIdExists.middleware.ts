5855413631216a146aa79fc3f90b19fe4fe05fcf
Amanda Dolci Figueiredo <amanda.dolci@gmail.com>
2025-03-09 14:28:12 -0300

    Cria DirectorMovie MiddleWare

---
+++ src/middlewares/verifyDirectorMovieIdExists.middleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors";
+import { DirectorMovieModel } from "../models";
+
+const verifyDirectorMovieIdExists = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
+  const directorMovieId: string = request.params.directorMovieId;
+
+  const directorMovie: DirectorMovieModel | null = await DirectorMovieModel.findByPk(
+    directorMovieId
+  );
+
+  if (!directorMovie) throw new AppError("DirectorMovie not found", 404);
+
+  response.locals = { ...response.locals, directorMovie };
+
+  return next();
+};
+
+export default verifyDirectorMovieIdExists;
