780ea32f623b8a8f21dba9a98fadb91167b91406
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-08 22:53:54 -0300

    Altera config do sequelize

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,7 +2,7 @@ import app from "..";
 import sequelize from "./database";
 
 sequelize
-  .sync({ alter: true })
+  .sync({ force: false })
   .then(() => {
     const PORT: number = Number(process.env.PORT || 3000);
     console.log("The database has been successfully synchronized.");
