0c2f78f372b8c21ba67a99f36af91116b6961263
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-15 19:02:35 -0300

    altera movie service

--- src/services/movie.services.ts
+++ src/services/movie.services.ts
@@ -1,14 +1,8 @@
-import {
-  MovieCreate,
-  MovieReturn,
-  MovieUpdate,
-  Pagination,
-  PaginationParams,
-} from "../interfaces";
+import { MovieCreate, MovieReturn, MovieUpdate, Pagination, PaginationParams } from "../interfaces";
 import { MovieModel } from "../models";
 import { ActorModel } from "../models/Actor.model";
 import { DirectorModel } from "../models/Director.model";
-import { movieReturnSchema } from "../schemas";
+import { movieCompleteReturnSchema, movieReturnSchema } from "../schemas";
 
 const getAllMovies = async ({
   page,
@@ -48,15 +42,43 @@ const getAllMovies = async ({
 
 const createMovie = async (payLoad: MovieCreate): Promise<MovieReturn> => {
   const movie = await MovieModel.create(payLoad);
+  const newMovie: MovieModel | null = await MovieModel.findByPk(movie.movieId, {
+    include: [
+      {
+        model: ActorModel,
+        as: "actors",
+        attributes: ["actorId", "name", "birthDate", "nationality"],
+      },
+      {
+        model: DirectorModel,
+        as: "directors",
+        attributes: ["directorId", "name", "birthDate", "nationality"],
+      },
+    ],
+  });
 
-  return movieReturnSchema.parse(movie);
+  return movieCompleteReturnSchema.parse(newMovie);
 };
 
 const updateMovie = async (movie: MovieModel, payLoad: MovieUpdate): Promise<MovieReturn> => {
   Object.assign(movie, payLoad);
   await movie.save();
+  const newMovie: MovieModel | null = await MovieModel.findByPk(movie.movieId, {
+    include: [
+      {
+        model: ActorModel,
+        as: "actors",
+        attributes: ["actorId", "name", "birthDate", "nationality"],
+      },
+      {
+        model: DirectorModel,
+        as: "directors",
+        attributes: ["directorId", "name", "birthDate", "nationality"],
+      },
+    ],
+  });
 
-  return movieReturnSchema.parse(movie);
+  return movieCompleteReturnSchema.parse(newMovie);
 };
 
 const deleteMovie = async (movie: MovieModel): Promise<void> => {
