d2c9f9808d362d46fea3ed55b70a8509c0492319
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-26 16:59:09 -0300

    Corrige tipagem

--- src/middlewares/pagination.middleware.ts
+++ src/middlewares/pagination.middleware.ts
@@ -1,8 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 const pagination = (request: Request, response: Response, next: NextFunction): void => {
-  const queryPage: number = Number(request.query.page);
-  const queryPerPage: number = Number(request.query.perPage);
+  const queryPage: number | undefined = Number(request.query.page);
+  const queryPerPage: number | undefined = Number(request.query.perPage);
 
   const page: number = queryPage && queryPage > 1 ? queryPage : 1;
   const perPage: number = queryPerPage && queryPerPage > 0 ? queryPerPage : 5;
@@ -10,6 +10,7 @@ const pagination = (request: Request, response: Response, next: NextFunction): v
   const baseUrl: string = `${request.protocol}://${request.get("host")}${request.baseUrl}${
     request.path
   }`;
+
   let prevPage: string | null = `${baseUrl}?page=${page - 1}&perPage=${perPage}`;
   const nextPage: string = `${baseUrl}?page=${page + 1}&perPage=${perPage}`;
 
