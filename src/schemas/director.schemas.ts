3910d8cf569bf883164fc6c6ffefa87d0221bb49
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-26 21:26:29 -0300

    Corrige schema de Director

--- src/schemas/director.schemas.ts
+++ src/schemas/director.schemas.ts
@@ -1,25 +1,37 @@
 import { z } from "zod";
+import { AppError } from "../errors";
+import { validateDate } from "../utils";
 
 const directorSchema = z.object({
   directorId: z.string().uuid(),
-  name: z
-    .string()
-    .min(1, "Director is required")
-    .max(100, "Director must be less than 100 characters"),
-  birthDate: z.string().min(10, "BirthDate is required").max(10, "BirthDate must be 10 characters"),
+  name: z.string().min(1, "Name is required").max(100, "Name must be less than 100 characters"),
+  birthDate: z.union([
+    z.string().refine((value) => {
+      const result = validateDate(value);
+      if (!result.isValid) throw new AppError(result.message!);
+      return true;
+    }),
+    z.null(),
+  ]),
   nationality: z
     .string()
     .min(1, "Nationality is required")
     .max(100, "Nationality must be less than 100 characters"),
 });
 
-const directorCreateSchema = directorSchema.omit({
-  directorId: true,
+const directorCreateSchema = directorSchema.omit({ directorId: true }).extend({
+  birthDate: z
+    .string()
+    .refine((value) => {
+      const result = validateDate(value);
+      if (!result.isValid) throw new AppError(result.message!);
+      return true;
+    })
+    .optional(),
 });
 
 const directorReturnSchema = directorSchema;
 const directorReadSchema = directorReturnSchema.array();
-
 const directorUpdateSchema = directorCreateSchema.partial();
 
 export {
