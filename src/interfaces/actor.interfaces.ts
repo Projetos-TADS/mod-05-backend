1c73279e766e1c1eb57a5a67e2c677631b09d394
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-26 21:25:35 -0300

    Corrige interfaces de Actor

--- src/interfaces/actor.interfaces.ts
+++ src/interfaces/actor.interfaces.ts
@@ -14,7 +14,7 @@ type ActorUpdate = DeepPartial<ActorCreate>;
 interface ActorAttributes {
   actorId: string;
   name: string;
-  birthDate: string;
+  birthDate: string | null;
   nationality: string;
 }
 
