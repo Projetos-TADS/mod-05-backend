9e5ec4ad6b353c4e6331a73113d4814bfdb06cb6
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-03-26 21:26:21 -0300

    Corrige schema de Actor

--- src/schemas/actor.schemas.ts
+++ src/schemas/actor.schemas.ts
@@ -1,22 +1,37 @@
 import { z } from "zod";
+import { AppError } from "../errors";
+import { validateDate } from "../utils";
 
 const actorSchema = z.object({
   actorId: z.string().uuid(),
   name: z.string().min(1, "Name is required").max(100, "Name must be less than 100 characters"),
-  birthDate: z.string().min(10, "BirthDate is required").max(10, "BirthDate must be 10 characters"),
+  birthDate: z.union([
+    z.string().refine((value) => {
+      const result = validateDate(value);
+      if (!result.isValid) throw new AppError(result.message!);
+      return true;
+    }),
+    z.null(),
+  ]),
   nationality: z
     .string()
     .min(1, "Nationality is required")
     .max(100, "Nationality must be less than 100 characters"),
 });
 
-const actorCreateSchema = actorSchema.omit({
-  actorId: true,
+const actorCreateSchema = actorSchema.omit({ actorId: true }).extend({
+  birthDate: z
+    .string()
+    .refine((value) => {
+      const result = validateDate(value);
+      if (!result.isValid) throw new AppError(result.message!);
+      return true;
+    })
+    .optional(),
 });
 
 const actorReturnSchema = actorSchema;
 const actorReadSchema = actorReturnSchema.array();
-
 const actorUpdateSchema = actorCreateSchema.partial();
 
 export { actorSchema, actorCreateSchema, actorUpdateSchema, actorReturnSchema, actorReadSchema };
