6224c130ee0f787009f660032276af2bf9d8521a
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-04-04 21:49:04 -0300

    atualiza sequelize.config

--- src/config/sequelize.config.ts
+++ src/config/sequelize.config.ts
@@ -16,6 +16,18 @@ const config: { [key: string]: Options } = {
       decimalNumbers: true,
     },
   },
+  test: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  },
 };
 
-export = config;
+export default config;
