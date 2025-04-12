b8f1a8cd4105517bad574d65ce79eeda12ba3388
Vinicius Garcia <advviniciusgarcia@gmail.com>
2025-04-12 11:18:26 -0300

    Atualiza interfaces de pagination

--- src/interfaces/pagination.interfaces.ts
+++ src/interfaces/pagination.interfaces.ts
@@ -1,10 +1,13 @@
 import { MovieModel } from "../models";
+import { ActorRead } from "./actor.interfaces";
+import { DirectorRead } from "./director.interfaces";
+import { UserRead } from "./user.interfaces";
 
 interface Pagination {
   prevPage: string | null;
   nextPage: string | null;
   count: number;
-  data: Array<MovieModel>;
+  data: Array<MovieModel> | ActorRead | DirectorRead | UserRead;
 }
 
 interface PaginationParams {
